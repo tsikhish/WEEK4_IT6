8ef9ba078111d367da90c5ff953aaa5fc2056833
tsikhish <tsikhishvilimariam@gmail.com>
2023-12-12 19:25:48 +0400

    my first visual studio code

---
+++ WEEK4_IT6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WEEK4_IT6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
